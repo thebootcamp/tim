0cea73ea8748b83f02a83045d66f98ea00790b90
jamey-austin-cengage <jamey.austin@cengage.com>
2024-10-31 13:51:01 -0500

    added sentiment analysis to chatbot

--- incident_bot/actions.py
+++ incident_bot/actions.py
@@ -1,5 +1,6 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 class ActionSubmitReport(Action):
     def name(self):
@@ -11,3 +12,26 @@ class ActionSubmitReport(Action):
         # Process the incident report...
         dispatcher.utter_message(text="Your report has been submitted. Our team will contact you shortly.")
         return []
+
+class ActionAnalyzeSentiment(Action):
+
+    def name(self) -> Text:
+        return "action_analyze_sentiment"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        analyzer = SentimentIntensityAnalyzer()
+        user_message = tracker.latest_message.get('text')
+        sentiment = analyzer.polarity_scores(user_message)
+
+        if sentiment['compound'] >= 0.05:
+            sentiment_label = "positive"
+        elif sentiment['compound'] <= -0.05:
+            sentiment_label = "negative"
+        else:
+            sentiment_label = "neutral"
+
+        # Store sentiment in a slot
+        return [SlotSet("sentiment", sentiment_label)]
--- incident_bot/actions/actions.py
+++ incident_bot/actions/actions.py
@@ -1,27 +1,37 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+class ActionSubmitReport(Action):
+    def name(self):
+        return "action_submit_report"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher, tracker, domain):
+        incident_type = tracker.get_slot('incident_type')
+        device = tracker.get_slot('device')
+        # Process the incident report...
+        dispatcher.utter_message(text="Your report has been submitted. Our team will contact you shortly.")
+        return []
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionAnalyzeSentiment(Action):
+
+    def name(self) -> Text:
+        return "action_analyze_sentiment"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        analyzer = SentimentIntensityAnalyzer()
+        user_message = tracker.latest_message.get('text')
+        sentiment = analyzer.polarity_scores(user_message)
+
+        if sentiment['compound'] >= 0.05:
+            sentiment_label = "positive"
+        elif sentiment['compound'] <= -0.05:
+            sentiment_label = "negative"
+        else:
+            sentiment_label = "neutral"
+
+        # Store sentiment in a slot
+        return [SlotSet("sentiment", sentiment_label)]
